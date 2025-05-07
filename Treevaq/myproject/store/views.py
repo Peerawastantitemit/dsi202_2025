77830b69862c80e13eb4efb58749500cd92195b8
pong <pong.peerwas@gmail.com>
2025-05-07 15:04:53 +0700

    add calculate co2

--- Treevaq/myproject/store/views.py
+++ Treevaq/myproject/store/views.py
@@ -3,4 +3,6 @@ from .models import Product
 
 def index(request):
     products = Product.objects.all()
-    return render(request, 'store/index.html', {'products': products})
+    total_co2 = 0  # ตัวอย่างการคำนวณ CO2
+    # ถ้ามีตะกร้า จะคำนวณจากสินค้าที่เลือก
+    return render(request, 'store/index.html', {'products': products, 'total_co2': total_co2})
