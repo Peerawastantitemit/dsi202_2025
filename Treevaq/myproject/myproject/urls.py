01894927512ae85d9df19575a04597b4b431b119
pong <pong.peerwas@gmail.com>
2025-05-07 15:02:06 +0700

    set urls

--- Treevaq/myproject/myproject/urls.py
+++ Treevaq/myproject/myproject/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('tadmin/', admin.site.urls),
+    path('', include('store.urls')),
 ]
